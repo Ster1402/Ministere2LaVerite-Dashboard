70db368e49c2f699b72dc3ee6b03fae82cbf2970
NDE TSAPI STEVE-ROLAND <ndetsapisteveroland@gmail.com>
2025-04-04 03:04:41 +0100

    feat: dynamic filtering update 2

--- app/View/Components/FilterManager.php
+++ app/View/Components/FilterManager.php
@@ -11,18 +11,24 @@ class FilterManager extends Component
     public $filters;
     public $attributes;
     public $operators;
+    public $action;
+    public $method;
 
     /**
      * Create a new component instance.
      *
-     * @param string $modelName
-     * @param array $filters
+     * @param string $modelName The model name to filter
+     * @param array $filters Pre-existing filters
+     * @param string $action Form action URL
+     * @param string $method Form method (GET/POST)
      * @return void
      */
-    public function __construct($modelName, $filters = [])
+    public function __construct($modelName, $filters = [], $action = null, $method = 'GET')
     {
         $this->modelName = $modelName;
         $this->filters = $filters;
+        $this->action = $action ?? request()->url();
+        $this->method = $method;
 
         // Load attributes and operators for the model
         $this->loadModelData();
@@ -59,12 +65,29 @@ class FilterManager extends Component
     }
 
     /**
+     * Check if the model is filterable
+     *
+     * @return bool
+     */
+    public function isFilterable()
+    {
+        return !empty($this->attributes) && !empty($this->operators);
+    }
+
+    /**
      * Get the view / contents that represent the component.
      *
      * @return \Illuminate\Contracts\View\View|\Closure|string
      */
     public function render()
     {
-        return view('components.filter-manager');
+        return view('components.filter-manager', [
+            'modelName' => $this->modelName,
+            'filters' => $this->filters,
+            'attributes' => $this->attributes,
+            'operators' => $this->operators,
+            'action' => $this->action,
+            'method' => $this->method
+        ]);
     }
 }
